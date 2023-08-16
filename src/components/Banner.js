99a8583d345345517e2c4301012e8440a9974e7c
Nibha <nibhamaharjan@yahoo.com>
2023-08-16 19:49:39 -0400

    Fix?

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,39 +5,44 @@ import headerImage from "../assets/img/header-img.svg";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleteing] = useState(false);
-    const toRotate = ["Web Developer" , "Mobile Application Developer" , "Third one"];
-    const [text, setText]= useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const period= 2000;
-    
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const period = 2000;
+  
     useEffect(() => {
-        let ticker = setInterval(()=>{
-            tick();
-        },delta()
-        )
-        return () => { clearInterval(ticker) };
-    }, [text])  
-    
-
+      let ticker = setInterval(() => {
+        tick();
+      }, delta);
+  
+      return () => { clearInterval(ticker) };
+    }, [text])
+  
     const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1);
-
-        setText(updatedText);
-        if (isDeleting){
-            setDelta(prevDelta => prevDelta/2)
-
-        }
-        if (!isDeleting && updatedText === fullText){
-            setIsDeleteing(true);
-            setDelta(period);
-        } else if (isDeleting && updatedText === ''){
-            setIsDeleteing(false);
-            setLoopNum(loopNum + 1);
-            setDelta(500);
-        }
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  
+      setText(updatedText);
+  
+      if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+      }
+  
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setIndex(prevIndex => prevIndex - 1);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setIndex(1);
+        setDelta(500);
+      } else {
+        setIndex(prevIndex => prevIndex + 1);
+      }
     }
 
     return (
